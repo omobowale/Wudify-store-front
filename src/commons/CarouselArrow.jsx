c5d4dd901b33709c75f0dd04b21158b48c4d5f61
Omobowale <omobowale92@outlook.com>
2024-04-01 02:04:58 +0100

    Added hover to carousel arrows

--- src/commons/CarouselArrow.jsx
+++ src/commons/CarouselArrow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CarouselArrow({children, onClick}) {
   return (
-    <div onClick={onClick} className="cursor-pointer p-1.5 flex items-center justify-center rounded-full bg-gray-300 opacity">
+    <div onClick={onClick} className="cursor-pointer p-1.5 flex items-center justify-center rounded-full bg-gray-300 hover:bg-black hover:text-white">
       {children}
     </div>
   );
